7d60c9196b847c2ee0d638b4cde26ace2387abed
Nikita Shishmarov <nshishmarov@diasoft.ru>
2026-05-15 14:56:30 +0300

    tech: updated neovim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("plugins.example")
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.softtabstop = 4
+vim.opt.autoindent = true
+vim.opt.smartindent = true
--- nvim/lua/plugins/example.lua
+++ nvim/lua/plugins/example.lua
@@ -13,13 +13,25 @@ return {
 
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
-  {"rebelot/kanagawa.nvim"},
+  {
+    "rebelot/kanagawa.nvim",
+    priority = 1000,
+    opts = {
+      theme = "wave",
+      background = {
+        dark = "dragon",
+        light = "lotus",
+        },
+    },
+  },
+
+  { "ellisonleao/gruvbox.nvim" },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa",
+      colorscheme = "tokyonight-night",
     },
   },
 
@@ -123,6 +135,19 @@ return {
   },
 
   {
+    "vinnymeller/swagger-preview.nvim",
+    cmd = { "SwaggerPreview", "SwaggerPreviewStop", "SwaggerPreviewToggle" },
+    build = "npm install", -- Ensure you have npm installed
+    opts = {
+      port = 8000,
+      host = "localhost",
+    },
+    keys = {
+      { "<leader>cp", "<cmd>SwaggerPreviewToggle<cr>", desc = "Toggle Swagger Preview" },
+    },
+  },
+
+  {
     "mfussenegger/nvim-dap",
     opts = function()
       local dap = require("dap")
@@ -139,4 +164,52 @@ return {
       })
     end,
   },
+
+    {
+        "Isrothy/neominimap.nvim",
+        version = "v3.x.x",
+        lazy = false, -- NOTE: NO NEED to Lazy load
+        -- Optional. You can also set your own keybindings
+        keys = {
+            -- Global Minimap Controls
+            { "<leader>nm", "<cmd>Neominimap Toggle<cr>", desc = "Toggle global minimap" },
+            { "<leader>no", "<cmd>Neominimap Enable<cr>", desc = "Enable global minimap" },
+            { "<leader>nc", "<cmd>Neominimap Disable<cr>", desc = "Disable global minimap" },
+            { "<leader>nr", "<cmd>Neominimap Refresh<cr>", desc = "Refresh global minimap" },
+
+             -- Window-Specific Minimap Controls
+            { "<leader>nwt", "<cmd>Neominimap WinToggle<cr>", desc = "Toggle minimap for current window" },
+            { "<leader>nwr", "<cmd>Neominimap WinRefresh<cr>", desc = "Refresh minimap for current window" },
+            { "<leader>nwo", "<cmd>Neominimap WinEnable<cr>", desc = "Enable minimap for current window" },
+            { "<leader>nwc", "<cmd>Neominimap WinDisable<cr>", desc = "Disable minimap for current window" },
+
+            -- Tab-Specific Minimap Controls
+            { "<leader>ntt", "<cmd>Neominimap TabToggle<cr>", desc = "Toggle minimap for current tab" },
+            { "<leader>ntr", "<cmd>Neominimap TabRefresh<cr>", desc = "Refresh minimap for current tab" },
+            { "<leader>nto", "<cmd>Neominimap TabEnable<cr>", desc = "Enable minimap for current tab" },
+            { "<leader>ntc", "<cmd>Neominimap TabDisable<cr>", desc = "Disable minimap for current tab" },
+
+            -- Buffer-Specific Minimap Controls
+            { "<leader>nbt", "<cmd>Neominimap BufToggle<cr>", desc = "Toggle minimap for current buffer" },
+            { "<leader>nbr", "<cmd>Neominimap BufRefresh<cr>", desc = "Refresh minimap for current buffer" },
+            { "<leader>nbo", "<cmd>Neominimap BufEnable<cr>", desc = "Enable minimap for current buffer" },
+            { "<leader>nbc", "<cmd>Neominimap BufDisable<cr>", desc = "Disable minimap for current buffer" },
+
+            ---Focus Controls
+            { "<leader>nf", "<cmd>Neominimap Focus<cr>", desc = "Focus on minimap" },
+            { "<leader>nu", "<cmd>Neominimap Unfocus<cr>", desc = "Unfocus minimap" },
+            { "<leader>ns", "<cmd>Neominimap ToggleFocus<cr>", desc = "Switch focus on minimap" },
+        },
+        init = function()
+            -- The following options are recommended when layout == "float"
+            vim.opt.wrap = false
+            vim.opt.sidescrolloff = 36 -- Set a large value
+
+            --- Put your configuration here
+            ---@type Neominimap.UserConfig
+            vim.g.neominimap = {
+                auto_enable = true,
+            }
+        end,
+    },
 }
